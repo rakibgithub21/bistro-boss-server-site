6ff57ac81d163fcae2c1a01d3bc7e2eaf355ea07
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-20 05:40:43 +0600

    make another collection

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vq4rqer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        // ----------------------------------------------------------------
+        const menuCollection = client.db('bistroDB').collection('menu');
+        const reviewCollection = client.db('bistroDB').collection('reviews');
+        const cartCollection = client.db('bistroDB').collection('carts');
+        // -----------------------------------------------------------------
+
+        // menu collection:-----------------------------------
+
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+
+        // review collection:-------------------------------
+
+        app.get('/reviews', async (req, res) => {
+            const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+
+        // cart collection:----------------------------------
+
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem)
+            res.send(result)
+        })
+
+
+        // -----------------------------------------
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('boss is sitting')
+})
+
+app.listen(port, () => {
+    console.log(`Bistro boss is running on${port}`);
+})
