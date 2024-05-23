68403cc06e3d1f9bca6dd19e04c2a4d8a1c78362
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-23 10:27:48 +0600

    make admin and delete api

--- index.js
+++ index.js
@@ -32,6 +32,19 @@ async function run() {
 
 
         // user related api:
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user?.email }
@@ -40,6 +53,18 @@ async function run() {
                 return res.send({message:'user already exists',insertedId:null})
             }
             const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role:'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
             res.send(result)
         })
 
